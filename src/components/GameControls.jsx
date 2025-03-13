f44dc4780fd00dc48aed0a4cc3a213d768a429b0
mw-kj <jjawor@metawin.inc>
2025-03-13 11:44:34 +0000

    styles update

--- src/components/GameControls.jsx
+++ src/components/GameControls.jsx
@@ -61,7 +61,7 @@ export default function GameControls({
           >
             Reset Game
           </button>
-          <div className="text-xs md:text-lg font-bold text-white text-center p-3 bg-gray-700/50 backdrop-blur rounded-xl flex justify-center">
+          <div className="text-xs md:text-lg font-bold text-white text-center p-3 bg-gray-700/50 backdrop-blur rounded-xl flex items-center justify-center">
             Score: {score}
           </div>
         </div>
